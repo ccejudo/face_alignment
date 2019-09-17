92dc624f2cc9000fefe959f72de0f4f3e7eab71b
Octavio Garduza <millennium935@icloud.com>
2019-09-17 15:06:27 -0500

    Librerias, atributos y constructor

--- FaceAligner.cpp
+++ FaceAligner.cpp
@@ -1,3 +1,36 @@
-class FaceAligner{
+//Librerias
+#include <opencv2/opencv.hpp>
+#include <dlib/image_processing.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/image_io.h>
+#include <iostream>
 
+using namespace dlib;
+using namespace std;
+
+class FaceAligner{
+  //Atributos
+  private:
+     shape_predictor sp;
+     double x_desired_left_eye;
+     double y_desired_left_eye;
+     int desiredFaceWidth;
+     int desiredFaceHeight;
+
+  public:
+    FaceAligner(shape_predictor sp1, double x_desired_left_eye1, double y_desired_left_eye1, int desiredFaceWidth1, int desiredFaceHeight1){
+      sp = sp1;
+      x_desired_left_eye = x_desired_left_eye1;
+      y_desired_left_eye = y_desired_left_eye1;
+      desiredFaceWidth = desiredFaceWidth1;
+      desiredFaceHeight = desiredFaceHeight1;
+    }
+
+    //Falta el tipo de dato de rect
+    void align(Mat img, Mat gray, std::vector<rectangle> rect){
+       full_object_detection shape = sp(gray, rect);
+       shape = 
+    }
 }
--- Helpers.cpp
+++ Helpers.cpp
@@ -1,4 +1,7 @@
+//Librerias
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing.h>
 
 class Helpers{
-
+  
 }

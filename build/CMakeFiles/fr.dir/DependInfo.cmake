ffd8b23f48ff05c8e2db91800efde7e209c3f4d0
CristopherCM <cristophercejudo@Cristophers-MacBook-Pro.local>
2019-09-18 17:34:04 -0500

    build added

---
+++ build/CMakeFiles/fr.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/cristophercejudo/Desktop/ITESM/5º_Semestre/Software_Modelling/face_alignment/FaceAligner.cpp" "/Users/cristophercejudo/Desktop/ITESM/5º_Semestre/Software_Modelling/face_alignment/build/CMakeFiles/fr.dir/FaceAligner.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "../dlib-19.17/dlib/.."
+  "/usr/local/Cellar/opencv/4.1.1_2/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/cristophercejudo/Desktop/ITESM/5º_Semestre/Software_Modelling/face_alignment/build/dlib_build/dlib/CMakeFiles/dlib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/fr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fr.dir/FaceAligner.cpp.o"
+  "fr"
+  "fr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

67fb8a0601b335a834f7a4259b069d13db5d03dc
sabrisantana5 <A01025397@itesm.mx>
2019-10-05 08:39:11 -0500

    Nueva clase y main que rotan la cara

--- Main2.cpp
+++ Main2.cpp
@@ -12,19 +12,24 @@ using namespace std;
 int main(int argc, char** argv) {
 
   dlib::array<array2d<rgb_pixel>> alignedImage;
+  cv::Mat alignedImage2;
   frontal_face_detector detector;
   array2d<rgb_pixel> img;
   image_window window;
   image_window window_fixed;
 
+  cv::Mat imageMat = cv::imread(argv[1]);
+
   detector = get_frontal_face_detector();
 
   //Crear objeto Alineador
-  FaceAligner2 faceA2(250.0, 750.0 ,700.0);
+  //Estos valores permiten que la imagen salga así
+  FaceAligner2 faceA2(50.0, 600.0, 600.0,  600.0, 1500);
+  //FaceAligner2 faceA2(5.0, 80.0, 60.0,  80.0, 150);
 
   //Cargar imagen a partir de su dirección de archivo
   load_image(img, argv[1]);
-  window.set_image(img);
+  //window.set_image(img);
 
   //Detectar caras en imagen --> Esto lo hace el equipo 1
   std::vector<rectangle> dets = detector(img);
@@ -33,15 +38,16 @@ int main(int argc, char** argv) {
   //Si se detectó al menos 1 cara --> De nuevo esto lo hace el equipo 1
   if(dets.size() > 0){
     //Llamada al método align
-    alignedImage = faceA2.align(img, dets);
+    alignedImage2 = faceA2.alignCV(imageMat,img, dets);
+    //Aquí recortamos la imagen al valor requerido pero debe ir en la clase
+    cv::resize(alignedImage2, alignedImage2, cv::Size(150,150), 0, 0, cv::INTER_AREA);
+    cv::imwrite("faceAligned.jpg", alignedImage2);
 
     //tile_images convierte un arreglo de imágenes en 1 solo
     //set_image muestra la imagen en pantalla
-    window_fixed.set_image(tile_images(alignedImage));
-
-
+    //window_fixed.set_image(tile_images(alignedImage));
     //Suspender
-    cin.get();
+    //cin.get();
   }
   else{
     cout << "No face detected" << endl;

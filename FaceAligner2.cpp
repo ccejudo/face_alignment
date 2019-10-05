4b694908e32494e26c2899be506f77c14cac6703
sabrisantana5 <A01025397@itesm.mx>
2019-10-05 08:45:00 -0500

    Rota la cara

--- FaceAligner2.cpp
+++ FaceAligner2.cpp
@@ -4,6 +4,7 @@
 #include <dlib/image_processing.h>
 #include <dlib/opencv.h>
 #include <iostream>
+#include <math.h>
 
 using namespace dlib;
 using namespace std;
@@ -13,19 +14,21 @@ class FaceAligner2{
   //Atributos de la clase
   private:
     shape_predictor sp;
-    double xDesiredLeftEye;
-    double yDesiredLeftEye;
+    double DesiredLeftEye[2];
+    double DesiredRightEye[2];
     int desiredFaceWidth;
     int desiredFaceHeight;
 
   public:
 
     //Constructor
-    FaceAligner2(double xDesiredLeftEye1, double yDesiredLeftEye1, int desiredFaceWidth1){
+    FaceAligner2(double xDesiredLeftEye1, double yDesiredLeftEye1, double xDesiredRightEye1, double yDesiredRightEye1, int desiredFaceWidth1){
       //Guardar modelo de puntos faciales en la variable sp
       deserialize("shape_predictor_5_face_landmarks.dat") >> sp;
-      xDesiredLeftEye = xDesiredLeftEye1;
-   	  yDesiredLeftEye = yDesiredLeftEye1;
+      DesiredLeftEye[0] = xDesiredLeftEye1;
+   	  DesiredLeftEye[1] = yDesiredLeftEye1;
+      DesiredRightEye[0] = xDesiredRightEye1;
+   	  DesiredRightEye[1] = yDesiredRightEye1;
       desiredFaceWidth = desiredFaceWidth1;
       desiredFaceHeight = desiredFaceWidth1;
     }
@@ -37,6 +40,7 @@ class FaceAligner2{
 
     //Función para alinear --> Retorna un arreglo de pixeles
     //img es pasada por referencia
+
     dlib::array<array2d<rgb_pixel>> align(array2d<rgb_pixel> &img, std::vector<rectangle> dets){
       //Vector de objetos full_object_detection
       std::vector<full_object_detection> shapes;
@@ -60,30 +64,11 @@ class FaceAligner2{
 
       cout << "right eye start pixel position: " << shape.part(1) << endl;
       cout << "right eye end pixel position: " << shape.part(0) << endl;
-
-      //Para ver donde quedan los ojos de la nueva imagen
-
-      /*frontal_face_detector detector;
-      matrix<rgb_pixel> aligned = tile_images(face_chips);
-
-      std::vector<rectangle> dets2 = detector(aligned);
-      shape = sp(aligned, dets2[0]);
-
-      //Muestra las nuevas posiciones de los ojos
-
-      cout << "New Positions" << '\n';
-      cout << "left eye start pixel position: " << shape.part(2) << endl;
-      cout << "left eye end pixel position: " << shape.part(3) << endl;
-
-      cout << "right eye start pixel position: " << shape.part(1) << endl;
-      cout << "right eye end pixel position: " << shape.part(0) << endl;*/
-
-
-
       return face_chips;
      }
 
-     cv::Mat alignCV(array2d<rgb_pixel> &img, std::vector<rectangle> dets){
+     //Función de alinear con Open CV
+     cv::Mat alignCV(cv::Mat imageMat,array2d<rgb_pixel> &img, std::vector<rectangle> dets){
 
        int leftEyeCenter[2];
        int rightEyeCenter[2];
@@ -91,12 +76,11 @@ class FaceAligner2{
 
        cv::Mat image_aligned;
 
-       std::vector<full_object_detection> shapes;
+       //std::vector<full_object_detection> shapes;
 
        full_object_detection shape = sp(img, dets[0]);
 
-       shapes.push_back(shape);
-
+       //Puntos de los ojos
        leftEyeCenter[0] = midPoint(shape.part(2).x(), shape.part(3).x());
        leftEyeCenter[1] = midPoint(shape.part(2).y(), shape.part(3).y());
 
@@ -106,14 +90,75 @@ class FaceAligner2{
        imageMidPoint[0] = midPoint(leftEyeCenter[0], rightEyeCenter[0]);
        imageMidPoint[1] = midPoint(leftEyeCenter[1], rightEyeCenter[1]);
 
+       //Escala
+       double scale;
+       double Op[2];
+       double Oi[2];
+       double Opmagnitud;
+       double Oimagnitud;
 
 
-       return image_aligned;
+       Op[0] = DesiredLeftEye[0] - DesiredRightEye[0];
+       Op[1] = DesiredLeftEye[1] - DesiredRightEye[1];
+
+       Oi[0] = leftEyeCenter[0] - rightEyeCenter[0];
+       Oi[1] = leftEyeCenter[1] - rightEyeCenter[1];
+
+       Opmagnitud = sqrt(pow(Op[0],2) + pow(Op[1],2));
+       Oimagnitud = sqrt(pow(Oi[0],2) + pow(Oi[1],2));
+
+       scale = Opmagnitud / Oimagnitud;
+
+       //Ángulo
+       double dY, dX;
+       double angle;
+       dY = rightEyeCenter[1] - leftEyeCenter[1];
+       dX = rightEyeCenter[0] - leftEyeCenter[0];
+       angle = (atan2(dY, dX)*180/M_PI);
+
+
+      //Traslación
+      double traslacion[2];
+      //traslacion[0] = abs(DesiredLeftEye[0] - leftEyeCenter[0]);
+      //traslacion[1] = abs(DesiredLeftEye[1] - leftEyeCenter[1]);
+
+      //las traslaciones de arriba son las que importan, esto fue para probar
+      traslacion[0] = 0; //suma izquierda, menos derecha
+      traslacion[1] =400; //mas arriba derecha, menos abajo a la izq
+
+      //No sabemoos bien como funcione eyesCenterPoints
+      cv::Point2f eyesCenterPoints(traslacion[0],traslacion[1]);
+      //getRotationMatrix2D
+      cv::Mat imgMatrix = cv::getRotationMatrix2D(eyesCenterPoints, angle, scale);
+      //warpAffine
+      cv::warpAffine(imageMat, image_aligned, imgMatrix, cv::Size(desiredFaceWidth,desiredFaceHeight), cv::INTER_LINEAR, cv::BORDER_CONSTANT);
+
+
+      return image_aligned;
      }
 
+//Funcion para sacar punto medio
      int midPoint(int x1, int x2){
        int midPoint = (x1 + x2)/2;
 
        return midPoint;
      }
 };
+
+
+//Para ver donde quedan los ojos de la nueva imagen
+
+/*frontal_face_detector detector;
+matrix<rgb_pixel> aligned = tile_images(face_chips);
+
+std::vector<rectangle> dets2 = detector(aligned);
+shape = sp(aligned, dets2[0]);
+
+//Muestra las nuevas posiciones de los ojos
+
+cout << "New Positions" << '\n';
+cout << "left eye start pixel position: " << shape.part(2) << endl;
+cout << "left eye end pixel position: " << shape.part(3) << endl;
+
+cout << "right eye start pixel position: " << shape.part(1) << endl;
+cout << "right eye end pixel position: " << shape.part(0) << endl;*/
